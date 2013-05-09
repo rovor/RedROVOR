84c3ecb35af8e18edfc2a38b6be11cf08f5357cc
tmccombs <astrothayne@gmail.com>
2013-05-09 16:06:25 -0600

    modularized twoFrameBrowser
    BrowserWindow.css describes the style of the
    browser window
    BrowserWindow.js declares a class BrowserWindow and some
    helper functions that take care of the client side code
    and we added a template tag that will set up the html and a little
    bit of javascript needed to actually create the window. Inheriting from
    twoFrameBrowser takes care of the necessary js and css inclusions.

--- web/Rovor/dirmanage/templatetags/FSforms.py
+++ web/Rovor/dirmanage/templatetags/FSforms.py
@@ -19,5 +19,16 @@ def dirChooserDialog(id,callback,startPath="Raw/"):
     This is intended for use in other forms'''
     return {'id':id, 'callback':callback, 'startPath':startPath}
 
+@register.inclusion_tag('dirmanage/browseTag.html')
+def twoFrameBrowse(id,startPath='',js_name=None):
+    '''template tag for use in twoFrameBrowser.html and children
+    to put a two-frame browser in place, with id = id
+    and initilaly with a top path of startPath. 
+    By default it create a BroswerWindow object named id,
+    but the name of the BrowserWindow object can be overriden by 
+    passing in a value to js_name'''
+    return {'id':id, 'startPath':startPath, 'js_name': js_name or id}
+
+
 
 
