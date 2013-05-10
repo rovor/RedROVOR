4ebbaa0add5566396b028b43c49d3654d04f3063
tmccombs <astrothayne@gmail.com>
2013-05-10 16:36:17 -0600

    fixed makeZero, makeDark, and renameAll
    They are all working now, yipppeeee!

--- web/Rovor/reduction/action_views.py
+++ web/Rovor/reduction/action_views.py
@@ -15,24 +15,6 @@ import logging
 logger = logging.getLogger('Rovor')
 
 
-
-@login_required
-def renameAll(request):
-    '''rename all files in a folder to be .fit instead of .FIT'''
-    #return HttpResponse(json.dumps(request.POST),mimetype='application/json')
-    if request.method != 'POST' or not 'path' in request.POST:
-        logger.info('Invalid request on renameAll')
-        raise Http404 #should only be accessed by POST method, with a path argument
-    try:
-        path = Filesystem.getTruePath(request.POST['path'])
-        logger.info('Renaming files in: ' + path)
-    except (ValueError, Filesystem.DoesNotExist, IOError):
-        logger.info('Attempted access to invalid path: ' + request.POST['path'])
-        return HttpResponse('{"ok":false,"error":"Invalid path"}',mimetype='application/json')
-    renamer.renameAll(path) #rename the files
-    return HttpResponse('{"ok":true}',mimetype='application/json')
-
-
 def process_path(request, block,mimetype='application/json'):
     '''helper function to abstract common code when accessing 
     a path over POST method. This will check that the method is POST
@@ -43,17 +25,24 @@ def process_path(request, block,mimetype='application/json'):
     given mimetype (defaults to json)'''
     if request.method != 'POST':
         logger.info('Non-POST attempt to acces POST-only resource')
-        #raise Http404
-    if 'path' in request.REQUEST:
+        raise Http404
+    if 'path' in request.POST:
         try:
-            path = Filesystem.getTruePath(request.REQUEST['path'])
+            path = Filesystem.getTruePath(request.POST['path'])
         except (ValueError, Filesystem.DoesNotExist, IOError):
-            logger.info('Attempted access to invalid path: '+request.REQUEST['path'])
+            logger.info('Attempted access to invalid path: '+request.POST['path'])
             return HttpResponse('{"ok":false,"error":"Invalid path"}',mimetype='application/json')
         return HttpResponse(block(path),mimetype=mimetype)
     else:
         return HttpResponse('{"ok":false,"error":"No Path supplied"}',mimetype='application/json')
-        
+
+@login_required
+def renameAll(request):
+    '''rename all files in a folder to be .fit instead of .FIT'''
+    def rename_func(path):
+        renamer.renameAll(path) #rename the files
+        return '{"ok":true}'
+    return process_path(request,rename_func)
 
 
 @login_required
