60d189dc109514816112252da69e5fe1ad218fb5
tmccombs <astrothayne@gmail.com>
2013-05-13 17:12:51 -0600

    fixed whitespace

--- redrovor/obsRecord.py
+++ redrovor/obsRecord.py
@@ -14,25 +14,25 @@ logger = logging.getLogger("redrovor.recordobs")
 dateRegex = re.compile(r'(\d{4}-\d{2}-\d{2})T.*')
 
 def recordObservation(fitsHeader,fname=''):
-	'''record the information contained in the header to the online database
-	and optionally the filename passed as fname'''
-	objName = frameTypes.getObjectName(fitsHeader)
-	obj = obsDB.obj_get_or_add(objName)
-	objid = obj['id']
-	ffilter = fitsHeader['FILTER']
-	exptime = fitsHeader['EXPTIME']
-	temp = fitsHeader['CCD-TEMP']
-	utdate = dateRegex.match(fitsHeader['DATE-OBS']).group(1)
-	
-	obsDB.newObservation(objid,utdate,ffilter,exptime,temp,fname=os.path.realpath(fname))
-	return
+    '''record the information contained in the header to the online database
+    and optionally the filename passed as fname'''
+    objName = frameTypes.getObjectName(fitsHeader)
+    obj = obsDB.obj_get_or_add(objName)
+    objid = obj['id']
+    ffilter = fitsHeader['FILTER']
+    exptime = fitsHeader['EXPTIME']
+    temp = fitsHeader['CCD-TEMP']
+    utdate = dateRegex.match(fitsHeader['DATE-OBS']).group(1)
+
+    obsDB.newObservation(objid,utdate,ffilter,exptime,temp,fname=os.path.realpath(fname))
+    return
 
 def recordDir(dir):
-	'''record information for all fits files of images in 
-	the given directory and subdirectories'''
-	for root, dirs, files in os.walk(dir):
-		for f in files:
-			if frameTypes.isFits(f):
+    '''record information for all fits files of images in 
+    the given directory and subdirectories'''
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            if frameTypes.isFits(f):
                 try:
                     header = pyfits.getheader(f)
                     if frameTypes.getFrameType(header) != 'object':
@@ -41,4 +41,3 @@ def recordDir(dir):
                 except Exception as e:
                     logger.error(e)
                     continue #keep going and record everything else
-				
