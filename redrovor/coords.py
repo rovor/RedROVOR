cf998050e0e215b15655bb76f46a6ad0ac871de1
tmccombs <astrothayne@gmail.com>
2013-05-21 15:14:24 -0600

    added option string to coords.py

--- redrovor/coords.py
+++ redrovor/coords.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python
 
 import os
+from collections import namedtuple
 
-def astrometrySolve(fname):
+
+Coords = namedtuple('Coords',['ra','dec'])
+
+def astrometrySolve(fname,**kwargs):
 	'''use the framework from astrometry.net to apply 
 	world coordinate systems to the file located at fname'''
-	os.system("/usr/local/astrometry/bin/solve-field {0}".format(fname))
+	return os.system("/usr/local/astrometry/bin/solve-field {0} {1}".format(buildOptions(kwargs),fname))
+
+
+
+
+
+def buildOptions(args):
+    '''build a string for the options to the solve-field command, this should not be
+    used directly, but as a helper for astrometrySolve'''
+    if 'options' in args:
+        optionStr = args['options']
+    else:
+        optionStr = ''
+    if 'guess' in args:
+        ra,dec = args['guess']
+        radius = args.get('radius',2)
+        optionStr += "--ra {0} --dec {1} --radius {2}".format(ra,dec,radius)
+    if 'lowscale' in args:
+        optionStr += "--scale-low " + args['lowscale']
+    if 'highscale' in args:
+        optionStr += "--scale-high " + args['highscale']
+    return optionStr
+
 
