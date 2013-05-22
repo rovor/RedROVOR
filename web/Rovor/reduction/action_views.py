6a8be835f4377b61c119d7a506e922d407d12032
tmccombs <astrothayne@gmail.com>
2013-05-22 16:59:37 -0600

    refactored action_views.py to use the class instead of the function process_path

--- web/Rovor/reduction/action_views.py
+++ web/Rovor/reduction/action_views.py
@@ -11,7 +11,7 @@ from redrovor.process import makeZero
 from redrovor.zerodarkprocess import ZeroDarkProcessor, doFirstPass
 from redrovor.secondpass import SecondPassProcessor, doSecondPass
 from dirmanage.models import Filesystem
-from dirmanage.toolset import process_path
+from dirmanage.toolset import PathProcessView, process_path
 
 import logging
 logger = logging.getLogger('Rovor')
@@ -19,86 +19,142 @@ logger = logging.getLogger('Rovor')
 
 
 @login_required
-def renameAll(request):
+@PathProcessView.pathOnly
+def renameAll(path):
     '''rename all files in a folder to be .fit instead of .FIT'''
-    def rename_func(path):
-        renamer.renameAll(path) #rename the files
-        return None
-    return process_path(request,rename_func)
+    renamer.renameAll(path) #rename the files
+    return HttpResponse('{"ok":true}',mimetype='application/json')
 
 
 @login_required
-def makeZero(request):
+@PathProcessView.pathOnly
+def makeZero(path):
     '''make a master zero from a folder'''
-    def zmaker(path):
+    try:
         improc = ZeroDarkProcessor(path)
         logger.info("Making zero at path " + path)
         improc.makeZero()
-        return None
-    return process_path(request,zmaker)
+        return HttpResponse('{"ok":true}',mimetype='application/json')
+    except ValueError as e:
+        return HttpResponse('{{"ok":false, "error":"{0}"}}'.format(e),mimetype='application/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
 
 @login_required
-def makeDark(request):
+@PathProcessView.pathOnly
+def makeDark(path):
     '''make a master dark from a folder'''
-    def dmaker(path):
+    try:
         improc = ZeroDarkProcessor(path)
         logger.info("Making dark at path " + path)
         improc.makeDark()
-        return None
-    return process_path(request,dmaker)
+        return HttpResponse('{"ok":true}',mimetype='applicatin/json')
+    except ValueError as e:
+        return HttpResponse('{{"ok":false, "error":"{0}"}}'.format(e), mimetype='application/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
 
 @login_required
-def makeFlats(request):
+@PathProcessView.pathOnly
+def makeFlats(path):
     '''make master flats from a folder'''
-    def fmaker(path):
+    try:
         improc = ZeroDarkProcessor(path)
         logger.info("Making flats at path " + path)
         improc.makeFlats()
-        return None
-    return process_path(request,fmaker)
+        return HttpResponse('{"ok":true}',mimetype='applicatin/json')
+    except ValueError as e:
+        return HttpResponse('{{"ok":false, "error":"{0}"}}'.format(e), mimetype='application/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
 
 @login_required
-def subZeroDark(request):
+@PathProcessView.pathOnly
+def subZeroDark(path):
     '''subtract zeros and darks from the object files in a folder'''
-    def subber(path):
+    try:
         improc = ZeroDarkProcessor(path)
         logger.info("Processing object files in "+path)
         improc.zero_and_dark_subtract()
-        return None
-    return process_path(request,subber)
+        return HttpResponse('{"ok":true}',mimetype='applicatin/json')
+    except ValueError as e:
+        return HttpResponse('{{"ok":false, "error":"{0}"}}'.format(e), mimetype='application/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
 
 @login_required
-def firstPass(request):
+@PathProcessView.pathOnly
+def firstPass(path):
     '''perform the first pass over the folder
     i.e. create calibration frames and apply zeros 
     and darks to the object frames, and save them in the processed folder
     '''
-    return process_path(request, doFirstPass)
+    try:
+        doFirstPass(path)
+        return HttpResponse('{"ok":true}',mimetype='applicatin/json')
+    except ValueError as e:
+        return HttpResponse('{{"ok":false, "error":"{0}"}}'.format(e), mimetype='application/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
+        
 
-
-def applyFlats(request):
+@login_required
+@PathProcessView.decorate
+def applyFlats(request,path):
     '''apply the given flats to the supplied path'''
-    def applier(path):
+    try:
         flats = json.loads(request.POST['flats'])
         for filt,flat in flats.items():
             flats[filt] = Filesystem.getTruePath(flat)
         improc = SecondPassProcessor(path)
         improc.applyFlats(flats)
-    return process_path(request,applier)
+        return HttpResponse('{"ok":true}',mimetype='applicatin/json')
+    except ValueError as e:
+        return HttpResponse('{{"ok":false, "error":"{0}"}}'.format(e), mimetype='application/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
 
-def applyWCS(request):
+@login_required
+@PathProcessView.pathOnly
+def applyWCS(path):
     '''apply world coordinate system to 
     images'''
-    def applier(path):
+    try:
         improc = SecondPassProcessor(path)
         improc.applyWCS()
-    return process_path(request,applier)
+        return HttpResponse('{"ok":true}',mimetype='applicatin/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
 
+@login_required
+@PathProcessView.pathOnly
 def secondPass(request):
     '''perform the second pass over the folder
     i.e. apply flats and apply world coordinate system
     '''
-    return process_path(request,doSecondPass)
+    try:
+        doSecondPass(path)
+        return HttpResponse('{"ok":true}',mimetype='applicatin/json')
+    except ValueError as e:
+        return HttpResponse('{{"ok":false, "error":"{0}"}}'.format(e), mimetype='application/json')
+    except Exception as e:
+        #an unexpected exception
+        logger.debug(traceback.format_exc()) #log the traceback
+        raise e
         
     
         
