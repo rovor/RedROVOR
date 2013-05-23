a9c1f13ce5cd58b65a54d5674a71290461b35ce8
tmccombs <astrothayne@gmail.com>
2013-05-23 13:46:20 -0600

    added setup.py
    setup file to install redrovor package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+'''installation script to install the redrovor package'''
+
+from distutils.core import setup
+
+setup(name="RedROVOR",
+    version="0.2.0",
+    description="Automated Reduction software designed for the ROVOR observatory",
+    author="Thayne McCombs",
+    author_email="astrothayne@gmail.com",
+    url="https://github.com/tmccombs/RedROVOR",
+    packages=['redrovor'],
+    )
