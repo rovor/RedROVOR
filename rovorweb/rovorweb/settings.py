556627ff2bb09fb462cb8708d185f9502b0b173b
tmccombs <astrothayne@gmail.com>
2013-05-23 14:03:25 -0600

    deleted iraf stuff in redrovor folder and updated settings
    settings updated so we can use collectstatic

--- rovorweb/rovorweb/settings.py
+++ rovorweb/rovorweb/settings.py
@@ -1,5 +1,9 @@
 # Django settings for Rovor project.
 
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))   #get the directory of the project, up two levels
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -60,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
