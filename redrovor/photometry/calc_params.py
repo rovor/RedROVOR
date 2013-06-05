664de35d120c7d04c5d85d7cf4bd8961415fe180
tmccombs <astrothayne@gmail.com>
2013-06-05 17:57:15 -0600

    added function to calculate center and fwhm of peak
    NOTE: the fwhm is very dependent on the bin sizes,
    and if the bins are too small, then the data is two noisy
    and we get multiple zeros. I don't know what to do about this,
    should we try and fit a gaussion, or a poisson distribution thing?
    I just don't know what to do right now.

--- redrovor/photometry/calc_params.py
+++ redrovor/photometry/calc_params.py
@@ -2,6 +2,8 @@
 should only be used internally by the phot package'''
 
 import pywcs
+import numpy
+from scipy.interpolate import UnivariateSpline as uniSpline
 
 def getBox(image, center, size=100):
     ''' get a box centered at \p center with a size of \p size
@@ -19,13 +21,54 @@ def getBox(image, center, size=100):
     ra,dec = center
     #we need to convert to decimal degrees before doing transformation
     r = float(ra.toDegrees())
-    dec = float(dec.toDegrees())
+    d = float(dec.toDegrees())
     x,y = mywcs.wcs_sky2pix([(r,d)],0)[0]  #perform conversion
-    #get the top and left coordinates
-    top = int(y-size/2)
+    #get the bottom and left coordinates
+    bottom = int(y-size/2)
     left = int(x-size/2)
 
-    return image.data[left:left+size,top:top+size]
+    #numpy arrays are column major, so we give y vallues first
+    return image.data[bottom:bottom+size,left:left+size]
+
+
+def im_histogram(box,bins=1000):
+    '''get the histogram of a numpy array, return (x,y) where
+    x is the midpoints of the bins, and y is the number of pixels in each bin
+    @param bins the number of bins to use'''
+
+    y,bins = numpy.histogram(box,bins=bins)
+    x = (bins[1:]+bins[:-1])/2  #compute the average of each consecutive pair of elements
+    return (x,y)
+
+
+class MultiplePeakError(Exception):
+    '''more than one peak in the distribution'''
+    pass
+
+class NoPeakFoundError(Exception):
+    '''no peak in distribution'''
+    pass
+
+def center_and_fwhm(x,y):
+    '''calculate the full width half max of the function y(x),
+    and get the center of the thing
+    @returns (center, fwhm) where center is the x value of the center of the peak and fwhm is
+    the full width half max of the peak'''
+    midx = numpy.argmax(y) #get index of maximum
+    half_max = y[midx]/2 #get half the maximum
+    center = x[midx] #get the value of the center
+
+    s = uniSpline(x,y-half_max)  #create a spline of the data
+    roots = s.roots()  #get roots of the spline, i.e. place of half-max
+    if len(roots) > 2:
+        #too many roots
+        raise MultiplePeakError("There appears to be more than one peak")
+    elif len(roots) < 2:
+        raise NoPeakFoundError("There doesn't appear to be a proper peak")
+    else:
+        return (center, abs(roots[1]-roots[0]))
+    
+
 
 
 
